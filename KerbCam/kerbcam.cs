f00c766b4f6bb3a9eefa6406c6e24123b44dac40
John Beisley <greatred@gmail.com>
2013-02-19 20:22:47 +0000

    Add main window placeholder.

--- KerbCam/kerbcam.cs
+++ KerbCam/kerbcam.cs
@@ -21,6 +21,8 @@ namespace KerbCam {
 	public class KerbCam : MonoBehaviour
 	{
 		private bool isEnabled = false;
+
+		private MainWindow mainWindow;
 		
 		private int keyIndex = 0;
 		private AnimationCurve hdgCurve = null;
@@ -34,6 +36,9 @@ namespace KerbCam {
 		{
 			isEnabled = (FlightGlobals.fetch == null || FlightGlobals.ActiveVessel == null)
 				&& HighLogic.LoadedScene == GameScenes.FLIGHT;
+
+			if (mainWindow == null)
+				mainWindow = new MainWindow();
 			
 			stopRunning();
 			
@@ -48,7 +53,7 @@ namespace KerbCam {
 			if (isRunning)
 			{
 				curTime += Time.deltaTime;
-				if (curTime > keyIndex)
+				if (curTime > (keyIndex-1))
 				{
 					stopRunning();
 				}
@@ -86,45 +91,82 @@ namespace KerbCam {
 			isRunning = false;
 		}
 		
-		public void OnGUI()
+		public void OnGUI ()
 		{
 			if (!isEnabled)
 				return;
-			
+
 			// Ignore input when running.
 			// TODO: Allow the sequence to be stopped.
 			if (isRunning)
 				return;
 			
 			// TODO: Custom keybindings.
-			if (Event.current.Equals(Event.KeyboardEvent("F4")))
-			{
+			if (Event.current.Equals(Event.KeyboardEvent("F4"))) {
 				var cam = FlightCamera.fetch;
 				// TODO: Proper GUI etc. so that timings can be tweaked.
 				// For now, each key is one second apart.
-				hdgCurve.AddKey(keyIndex, cam.camHdg);
-				pitchCurve.AddKey(keyIndex, cam.camPitch);
-				zoomCurve.AddKey(keyIndex, cam.Distance);
+				hdgCurve.AddKey (keyIndex, cam.camHdg);
+				pitchCurve.AddKey (keyIndex, cam.camPitch);
+				zoomCurve.AddKey (keyIndex, cam.Distance);
 				keyIndex++;
-			} else if (Event.current.Equals(Event.KeyboardEvent("F5")))
-			{
-				startRunning();
-			} else if (Event.current.Equals(Event.KeyboardEvent("F6")))
-			{
-				startCurve();
-			} else if (Event.current.Equals(Event.KeyboardEvent("F7")))
-			{
+			} else if (Event.current.Equals(Event.KeyboardEvent("F5"))) {
+				startRunning ();
+			} else if (Event.current.Equals(Event.KeyboardEvent("F6"))) {
+				startCurve ();
+			} else if (Event.current.Equals(Event.KeyboardEvent("F7"))) {
 				// TODO: Find out if we can use these for other pathing techniques.
-				Debug.Log (FlightCamera.fetch.transform.localPosition);
-				Debug.Log (FlightCamera.fetch.transform.localRotation);
-				Debug.Log (FlightCamera.fetch.transform.localScale);
-				Debug.Log (FlightCamera.fetch.transform.right);
-				Debug.Log (FlightCamera.fetch.transform.up);
-				Debug.Log (FlightCamera.fetch.targetDirection);
-				Debug.Log (FlightCamera.fetch.transform.forward);
-				Debug.Log (FlightCamera.fetch.transform.rotation);
-				Debug.Log (FlightCamera.fetch.camera);
+				Debug.Log(FlightCamera.fetch.transform.localPosition);
+				Debug.Log(FlightCamera.fetch.transform.localRotation);
+				Debug.Log(FlightCamera.fetch.transform.localScale);
+				Debug.Log(FlightCamera.fetch.transform.right);
+				Debug.Log(FlightCamera.fetch.transform.up);
+				Debug.Log(FlightCamera.fetch.targetDirection);
+				Debug.Log(FlightCamera.fetch.transform.forward);
+				Debug.Log(FlightCamera.fetch.transform.rotation);
+				Debug.Log(FlightCamera.fetch.camera);
+			} else if (Event.current.Equals(Event.KeyboardEvent("F8"))) {
+				mainWindow.ToggleWindow();
 			}
+		}
+	}
+
+	class MainWindow
+	{
+		private const int WINDOW_ID = 73469086; // xkcd/221 compliance.
+		private bool isWindowOpen = false;
+		private Rect windowPos;
+
+		public MainWindow()
+		{
+			windowPos = new Rect(Screen.width / 2, Screen.height / 2, 100, 50);
+		}
+
+		public void ToggleWindow()
+		{
+			isWindowOpen = !isWindowOpen;
+			if (isWindowOpen) {
+				RenderingManager.AddToPostDrawQueue(3, new Callback(DrawGUI));
+			} else {
+				RenderingManager.RemoveFromPostDrawQueue(3, new Callback(DrawGUI));
+			}
+		}
+
+		private void DrawGUI()
+		{
+			GUI.skin = HighLogic.Skin;
+			windowPos = GUILayout.Window(
+				WINDOW_ID, windowPos, DoGUI, "KerbCam",
+				GUILayout.MinWidth(100),
+				GUILayout.MinHeight(100));
+		}
+
+		private void DoGUI(int windowID)
+		{
+			GUILayout.BeginVertical();
+			GUILayout.Button("DO SOMETHING");
+			GUILayout.EndVertical();
+			GUI.DragWindow();
 		}
 	}
 }
